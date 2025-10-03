3ac7890b5d627834e90c67bf21e805a3ce592f0f
Jack <44051158+BJackal@users.noreply.github.com>
2025-10-03 13:18:39 +0100

    Update SpecifiedInputBashScript.sh
    correciton

--- SpecifiedInputBashScript.sh
+++ SpecifiedInputBashScript.sh
@@ -33,10 +33,10 @@ for ((i = 1; i <= $rec_column3; i += 1)); do
      echo "Run: $i"
      ## Running simulation with read in parameters and taking a random interger number between 1 - 1000 as the 5th command line option
      ## To prevent issues with the possibility of hanging simulations we introduce a watcher to check over the process and ensure it does not exceed a timeout.
-     ## Currentlly this timeout is defaulted to 30 minutes (108000 seconds), which is about ten mintues over the expected runtime just incase a simulation is running slowly.
+     ## Currentlly this timeout is defaulted to 30 minutes (1800 seconds), which is about ten mintues over the expected runtime just incase a simulation is running slowly.
      ## If a process exceeds this limit it will be killed and a corresponding warning will be output
      (~/build/projects/BayesianTissueProject2/test/TestPaperCommandLineSpeedSimulation --timeout 1 -opt1 $rec_column1 -opt2 $rec_column2 -opt3 $i -opt4 $rec_column4 -opt5 $((1+ $RANDOM % 1000))) & pid=$!
-     (sleep 108000 && kill -HUP $pid) 2>/dev/null & watcher=$!
+     (sleep 1800 && kill -HUP $pid) 2>/dev/null & watcher=$!
      if wait $pid 2>/dev/null; then
         echo "The simulation ran correctly"
         pkill -HUP -P $watcher
