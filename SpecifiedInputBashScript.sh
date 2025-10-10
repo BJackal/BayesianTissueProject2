6658c5201ac5a27b9a197b492480d03a63c14476
Jack <44051158+BJackal@users.noreply.github.com>
2025-10-10 14:04:38 +0100

    Update SpecifiedInputBashScript.sh
    Parallelising the watcher for each simulation run. Allows for all simulations to be ran in parallel. In addition any single simulation that fails due to reaching the timeout of 1800s (30 minutes) will be killed due to hanging. 
    
    Updating the "sleep 1800" value (in seconds) will change the tolerance of how long we enjoy a set of simulations to run. e.g for simulations running at an average of 35 minutes to complete you would need to increase the sleep to accomodate this.

--- SpecifiedInputBashScript.sh
+++ SpecifiedInputBashScript.sh
@@ -36,17 +36,16 @@ for ((i; i <= $rec_column3; i += 1)); do
      ## To prevent issues with the possibility of hanging simulations we introduce a watcher to check over the process and ensure it does not exceed a timeout.
      ## Currentlly this timeout is defaulted to 30 minutes (1800 seconds), which is about ten mintues over the expected runtime just incase a simulation is running slowly.
      ## If a process exceeds this limit it will be killed and a corresponding warning will be output
-     (~/build/projects/BayesianTissueProject2/test/TestPaperCommandLineSpeedSimulation --timeout 1 -opt1 $rec_column1 -opt2 $rec_column2 -opt3 $i -opt4 $rec_column4 -opt5 $((1+ $RANDOM % 1000))) & pid=$!
-     (sleep 1800 && kill -HUP $pid) 2>/dev/null & watcher=$!
+     ((~/build/projects/BayesianTissueProject2/test/TestPaperCommandLineSpeedSimulation --timeout 1 -opt1 $rec_column1 -opt2 $rec_column2 -opt3 $i -opt4 $rec_column4 -opt5 $((1+ $RANDOM % 1000))) & pid=$!
+     (sleep 1800 && kill -HUP $pid) 2>/dev/null & watcher=$! 
      if wait $pid 2>/dev/null; then
         ##echo "The simulation ran correctly"
         pkill -HUP -P $watcher
         wait $watcher
      else 
         echo "The simulation was killed due to exceeding the timout limit"
-        i=`expr $rec_column3`
-        echo "The Parameter pair of Lambda = $rec_column1 and Gamma = $rec_column2 will be skipped"
-     fi      
+        echo "The Parameter pair of Lambda = $rec_column1 and Gamma = $rec_column2 in Run $i failed due to timing out"
+     fi)&      
      echo ""
 done
 done < <(tail -n  +1 ~/Chaste/projects/BayesianTissueProject2/ExampleCommandLineCSV.csv)
